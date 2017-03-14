ec980861300dbd69151ebfa9c22f84cca7195428
David Tan <davidtan@thoughtworks.com>
2017-03-14 12:00:37 +0800

    Add redux to create-react-app boilerplate code

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import logo from '../images/logo.svg'
 import '../css/App.css'
 
+import PeopleContainer from './PeopleContainer';
+
 class App extends Component {
   render () {
     return (
@@ -13,6 +15,7 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <PeopleContainer />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom'
 import App from './components/App'
 import './css/index.css'
 
+import {Provider} from 'react-redux'
+import configureStore from './store/configure-store'
+
+const store = configureStore()
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 )
